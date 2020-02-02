6a2722177ee4d70adb6d0fef3c7762ba6189857e
Carlos Martinez <carlosed127@gmail.com>
2020-02-02 14:59:33 -0300

    implement navigation

--- app/src/main/java/com/hako/friendlists/view/HomeActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.hako.friendlists.view
-
-import androidx.appcompat.app.AppCompatActivity
-
-class HomeActivity : AppCompatActivity() {
-
-}
--- userlist/src/main/java/com/hako/friendlist/feature/UserlistFragment.kt
+++ userlist/src/main/java/com/hako/friendlist/feature/UserlistFragment.kt
@@ -1,4 +1,14 @@
 package com.hako.friendlist.feature
 
-class UserlistFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hako.friendlist_userlist.R
+
+class UserlistFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_userlist, container, false)
+    }
 }
